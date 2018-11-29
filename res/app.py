9aada63e2ad89499caa60bf856b6741c28bf336d
olszzak <olszzak@gmail.com>
2018-11-29 09:30:56 +0100

    adding to postgres

--- res/app.py
+++ res/app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlalchemy
 
 df = pd.read_csv('data/restaurant-scores-lives-standard.csv')
-engine = sqlalchemy.create_engine('postgresql://user:user@kaggle_db_1_e28d2ac2d53d/tabela')
+engine = sqlalchemy.create_engine('postgresql://user:user@kaggle_db/tabela')
 con = engine.connect()
 
 df.to_sql('restaurant2', con)
