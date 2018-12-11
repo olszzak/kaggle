3230b7adbc7eb3c7e04a1c08510836f6685e0aa7
olszzak <olszzak@gmail.com>
2018-12-11 11:41:24 +0100

    adding to postgres

--- res/app.py
+++ res/app.py
@@ -2,7 +2,17 @@ import pandas as pd
 import sqlalchemy
 
 df = pd.read_csv('data/restaurant-scores-lives-standard.csv')
-engine = sqlalchemy.create_engine('postgresql://user:user@db/tabela')
-con = engine.connect()
 
-df.to_sql('restaurant2', con)
+
+db = False
+while not db:
+    try:
+        engine = sqlalchemy.create_engine('postgresql://user:user@db/tabela')
+        con = engine.connect()
+        df.to_sql('restaurant2', con)
+    except:
+        db=False
+    else:
+        db=True
+
+
